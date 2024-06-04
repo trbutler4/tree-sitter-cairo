72c8d9f3acd53d10d3b328ddcfd06ca33c80bf5e
Thomas Butler <trbiv@proton.me>
2024-06-04 03:50:55 -0500

    updating tests to reflect changing func to fn

---
+++ build/Release/.deps/Release/obj.target/tree_sitter_cairo_binding/bindings/node/binding.o.d
@@ -0,0 +1,157 @@
+cmd_Release/obj.target/tree_sitter_cairo_binding/bindings/node/binding.o := c++ -o Release/obj.target/tree_sitter_cairo_binding/bindings/node/binding.o ../bindings/node/binding.cc '-DNODE_GYP_MODULE_NAME=tree_sitter_cairo_binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node -I/Users/iv-personal/Library/Caches/node-gyp/18.18.2/src -I/Users/iv-personal/Library/Caches/node-gyp/18.18.2/deps/openssl/config -I/Users/iv-personal/Library/Caches/node-gyp/18.18.2/deps/openssl/openssl/include -I/Users/iv-personal/Library/Caches/node-gyp/18.18.2/deps/uv/include -I/Users/iv-personal/Library/Caches/node-gyp/18.18.2/deps/zlib -I/Users/iv-personal/Library/Caches/node-gyp/18.18.2/deps/v8/include -I../node_modules/nan -I../src  -O3 -gdwarf-2 -mmacosx-version-min=10.15 -arch arm64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++17 -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/tree_sitter_cairo_binding/bindings/node/binding.o.d.raw   -c
+Release/obj.target/tree_sitter_cairo_binding/bindings/node/binding.o: \
+  ../bindings/node/binding.cc ../src/tree_sitter/parser.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/cppgc/common.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8config.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-array-buffer.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-local-handle.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-internal.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-version.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-object.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-maybe.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-persistent-handle.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-weak-callback-info.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-primitive.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-data.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-value.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-traced-handle.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-container.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-context.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-snapshot.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-date.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-debug.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-script.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-message.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-exception.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-extension.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-external.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-function.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-function-callback.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-template.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-memory-span.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-initialization.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-callbacks.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-isolate.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-embedder-heap.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-microtask.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-statistics.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-promise.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-unwinder.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-embedder-state-scope.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-platform.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-json.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-locker.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-microtask-queue.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-primitive-object.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-proxy.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-regexp.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-typed-array.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-value-serializer.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-wasm.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node_version.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node_api.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/js_native_api.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/js_native_api_types.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node_api_types.h \
+  ../node_modules/nan/nan.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv/errno.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv/version.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv/unix.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv/threadpool.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv/darwin.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node_buffer.h \
+  /Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node_object_wrap.h \
+  ../node_modules/nan/nan_callbacks.h \
+  ../node_modules/nan/nan_callbacks_12_inl.h \
+  ../node_modules/nan/nan_maybe_43_inl.h \
+  ../node_modules/nan/nan_converters.h \
+  ../node_modules/nan/nan_converters_43_inl.h \
+  ../node_modules/nan/nan_new.h \
+  ../node_modules/nan/nan_implementation_12_inl.h \
+  ../node_modules/nan/nan_persistent_12_inl.h \
+  ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+  ../node_modules/nan/nan_private.h \
+  ../node_modules/nan/nan_typedarray_contents.h \
+  ../node_modules/nan/nan_json.h ../node_modules/nan/nan_scriptorigin.h
+../bindings/node/binding.cc:
+../src/tree_sitter/parser.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/cppgc/common.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8config.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-array-buffer.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-local-handle.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-internal.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-version.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-object.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-maybe.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-persistent-handle.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-weak-callback-info.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-primitive.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-data.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-value.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-traced-handle.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-container.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-context.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-snapshot.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-date.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-debug.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-script.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-message.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-exception.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-extension.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-external.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-function.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-function-callback.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-template.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-memory-span.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-initialization.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-callbacks.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-isolate.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-embedder-heap.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-microtask.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-statistics.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-promise.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-unwinder.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-embedder-state-scope.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-platform.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-json.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-locker.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-microtask-queue.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-primitive-object.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-proxy.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-regexp.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-typed-array.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-value-serializer.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/v8-wasm.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node_version.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node_api.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/js_native_api.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/js_native_api_types.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node_api_types.h:
+../node_modules/nan/nan.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv/errno.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv/version.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv/unix.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv/threadpool.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/uv/darwin.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node_buffer.h:
+/Users/iv-personal/Library/Caches/node-gyp/18.18.2/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
+../node_modules/nan/nan_scriptorigin.h:
